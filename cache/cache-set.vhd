3e545b530967b2b11c4d96a9d46abfd6ea8f663c
poofypigeon <paige@liebster.ca>
2021-06-20 17:08:58 -0400

    idk

--- cache/cache-set.vhd
+++ cache/cache-set.vhd
@@ -13,15 +13,17 @@ entity cache_set is
     port(
         clk : in std_logic;
 
-        hit : out std_logic;
-        tag_query : in unsigned(tag_bit_width - 1 downto 0);
-        reset_valid : in std_logic
+        set_is_selected : in  std_logic;
+        reset_valid     : in  std_logic;
+        hit             : out std_logic;
+        tag_query       : in  unsigned(tag_bit_width - 1 downto 0)
     );
 end cache_set;
 
 architecture cache_set_arch of cache_set is
     constant block_count : positive := 2 ** block_id_bit_width;
 
+    signal pulse_s                : std_logic;
     signal all_blocks_valid_s     : std_logic;
     signal hit_s                  : std_logic;
     signal valid_blocks_s         : std_logic_vector(0 to block_count - 1);
@@ -30,13 +32,15 @@ architecture cache_set_arch of cache_set is
     signal block_to_replace_s     : std_logic_vector(0 to block_count - 1);
     signal replace_en_s           : std_logic_vector(0 to block_count - 1);
     signal hit_block_s            : std_logic_vector(0 to block_count - 1);
-    signal block_to_toggle_s      : std_logic_vector(0 to block_count - 1)
+    signal block_to_toggle_s      : std_logic_vector(0 to block_count - 1);
     
 begin
     gen_blocks : for i in 0 to block_count - 1 generate
         block_instance : entity work.cache_block
             generic map ( tag_bit_width => tag_bit_width )
             port map ( 
+                clk => pulse_s;
+
                 tag_query   => tag_query,
                 replace_en  => replace_en_s,
                 reset_valid => reset_valid,
@@ -56,15 +60,17 @@ begin
     main_policy : entity work.plru_policy
         generic map ( height => block_id_bit_width )
         port map (
-            clk => clk,
+            clk => pulse_s,
         
             toggle_in   => block_to_toggle_s;
             replace_out => plru_policy_replace_s
         );
 
+    pulse_s <= clk and set_is_selected;
+
     hit_s <= '1' when to_integer(unsigned(hit_block_s)) /= 0
         else '0';
-    hit   <= hit_s;
+    hit   <= hit_s and set_is_selected;
 
     block_to_toggle_s  <= hit_block_s when hit_s = '1'
                      else block_to_replace_s;
