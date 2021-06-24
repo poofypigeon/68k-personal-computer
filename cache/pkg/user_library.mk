0188f7cad88201c123a2570f02a4b40288898bdb
poofypigeon <paige@liebster.ca>
2021-06-24 19:49:47 -0400

    more makefile flexibility

--- cache/pkg/user_library.mk
+++ cache/pkg/user_library.mk
@@ -8,21 +8,22 @@
 # 		build library for encoder and its dependencies (one_hot_type, vector_tools)
 # > make flip_flop
 # 		build library for flip_flop and its dependency (vector_tools)
+# TODO these options could be moved to a README file
 
+# VHDL compiler
 GHDL = ghdl
+
 # object file names
 USR_LIB   	= user_library
 ENCODER   	= encoder
 FLIP_FLOP   = flip_flop
 ONE_HOT   	= one_hot
 VTOOLS 		= vector_tools
-
 # option values
-WORK_DIR  	= obj/
-STD 	 	= 93c
+STD = 93c
 
 # option flags
-OPTS = --std=${STD} --workdir=${WORK_DIR}
+OPTS = --std=${STD}
 
 # default - all libraries
 user_library : WORK = ${USR_LIB}
@@ -30,39 +31,33 @@ user_library : FILES = 	one-hot-type.vhd 	\
 						vector-tools.vhd 	\
 						encoder.vhd 		\
 						flip-flop.vhd 		
-user_library : ${WORK}-obj93.cf
+user_library : user_library-obj93.cf
 
-# one_hot_type only
 one_hot_type : WORK = ${ONE_HOT}
 one_hot_type : FILES = one-hot-type.vhd
-one_hot_type : ${WORK}-obj93.cf
-# vector_tools only
+one_hot_type : one_hot_type-obj93.cf
+
 vector_tools : WORK = ${VTOOLS}
 vector_tools : FILES = vector-tools.vhd
-vector_tools : ${WORK}-obj93.cf
+vector_tools : vector_tools-obj93.cf
 
-# encoder only
 encoder 	 : WORK = ${ENCODER}
 encoder 	 : FILES = 	one-hot-type.vhd	\
 						vector-tools.vhd 	\
 						encoder.vhd
-encoder 	 : ${WORK}-obj93.cf
-# flip_flop only
+encoder 	 : encoder-obj93.cf
+
 flip_flop 	 : WORK = ${FLIP_FLOP}
 flip_flop 	 : FILES = flip-flop.vhd
-flip_flop 	 : ${WORK}-obj93.cf
+flip_flop 	 : flip_flop-obj93.cf
 
-# make directory if it doesn't exist
-${WORK_DIR} :
-	@echo "Making directory ${WORK_DIR}..."
-	@mkdir ${WORK_DIR}
-
-${WORK}-obj93.cf : ${WORK_DIR} ${FILES}
+# analysis
+%-obj93.cf : ${FILES}
 	@echo "Analyzing files...";
-	@for file in ${FILES}; 								\
+	@for file in ${FILES}; 									\
 	do													\
 		echo " > \033[0;36m$$file\033[0m" ; 			\
 		${GHDL} -a --work=${WORK} ${OPTS} $$file ; 		\
 	done ;
-	@echo "Analysis finished : ${WORK_DIR}${WORK}-obj93.cf"
+	@echo "Analysis finished : ${WORK}-obj93.cf"
 
