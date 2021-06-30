50574d7599dc91c5edc417d0fc4879acc0b06389
poofypigeon <paige@liebster.ca>
2021-06-29 22:20:23 -0400

    output to file readable by textio

--- test/scripts/set_stim_gen.py
+++ test/scripts/set_stim_gen.py
@@ -23,32 +23,34 @@ if __name__ == "__main__":
     queries = gen_queries(0, 256)
     valid_iterator = iter(block_list)
 
-    for query in queries:
-        if randint(0, 7) != 0:
-            set_is_selected = 1
-            try:
-                block_id = block_list.index(query)
-                query_hit = 1
-            except ValueError:
-                query_hit = 0
-                update_block = next(valid_iterator, -1)
-                if update_block == -1:
-                    block_id = tree.bit_to_replace()
-                else:
-                    block_id = block_list.index(update_block)
-            finally:
+    with open("../stimulus/set.stim", 'w') as file:
+        for query in queries:
+            if randint(0, 7) != 0:
+                set_is_selected = 1
+                try:
+                    block_id = block_list.index(query)
+                    query_hit = 1
+                except ValueError:
+                    query_hit = 0
+                    update_block = next(valid_iterator, -1)
+                    if update_block == -1:
+                        block_id = tree.bit_to_replace()
+                    else:
+                        block_id = block_list.index(update_block)
+                finally:
+                    block_list[block_id] = query
+                    tree.toggle(block_id)
+            else:
+                set_is_selected = 0
+                try:
+                    block_id = block_list.index(query)
+                except ValueError:
+                    block_id = 0
+                finally:
+                    query_hit = 0
+
                 block_list[block_id] = query
                 tree.toggle(block_id)
-        else:
-            set_is_selected = 0
-            try:
-                block_id = block_list.index(query)
-            except ValueError:
-                block_id = 0
-            finally:
-                query_hit = 0
 
-            block_list[block_id] = query
-            tree.toggle(block_id)
-
-        print("set_is_selected: %1d; query: %3d; query_hit: %1d; hit_block_id: %2d" % (set_is_selected, query, query_hit, block_id))
+            print("set_is_selected: %d ; query: %-3d ; query_hit: %d ; hit_block_id: %-2d" % (set_is_selected, query, query_hit, block_id))
+            file.write("%d %-3d %d %-2d\n" % (set_is_selected, query, query_hit, block_id))
